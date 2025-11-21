c65236a170994ddaea6c053cc11ce9639254f134
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-21 09:39:20 +0300

    Editing configurations and moving directories

--- src/main/java/mg/fruive/configuration/ResourceConfig.java
+++ src/main/java/mg/fruive/configuration/ResourceConfig.java
@@ -11,7 +11,7 @@ public class ResourceConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		
 		registry.addResourceHandler("/images/**")
-				.addResourceLocations("file:./app/data/images/");
+				.addResourceLocations("file:./appdata/images/");
 		
 	}
 
