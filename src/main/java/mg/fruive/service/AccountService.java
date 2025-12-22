23939c49e8b2270fdbe5f61553297e44a6cb6322
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-12-22 07:46:09 +0300

    Removing Model from AccountService

--- src/main/java/mg/fruive/service/AccountService.java
+++ src/main/java/mg/fruive/service/AccountService.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-
 import lombok.AllArgsConstructor;
 import mg.fruive.entity.Account;
 import mg.fruive.exception.NotFoundException;
@@ -18,14 +16,12 @@ public class AccountService {
 	
 	private AccountRepository accountRepository;
 	
-	public List<Account> findAll(Model model) {
+	public List<Account> findAll() {
 		
-		List<Account> accounts = accountRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
-		
-		if(model != null)
-			model.addAttribute("accounts", accounts);
-		
-		return accounts;
+		return accountRepository.findAll(Sort.by(
+				Sort.Direction.ASC, 
+				"id"
+		));
 		
 	}
 	
