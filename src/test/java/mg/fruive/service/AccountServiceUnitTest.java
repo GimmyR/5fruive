fdca8288d1fc2098f974f12917ebceb9b02cb3d6
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-12-22 08:44:08 +0300

    Doing unit test on AccountService

---
+++ src/test/java/mg/fruive/service/AccountServiceUnitTest.java
@@ -0,0 +1,72 @@
+package mg.fruive.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import mg.fruive.entity.Account;
+import mg.fruive.entity.Role;
+import mg.fruive.exception.NotFoundException;
+import mg.fruive.repository.AccountRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountServiceUnitTest {
+	
+	@Mock
+	private AccountRepository accountRepository;
+	
+	@InjectMocks
+	private AccountService accountService;
+	
+	@Test
+	public void findAllShouldReturnAccounts() {
+		
+		List<Role> roles = Arrays.asList(new Role(1, "Client"));
+		List<Account> accounts = Arrays.asList(new Account(1, "John", "Doe", "johndoe", "mdpJohn", roles));
+		
+		when(accountService.findAll()).thenReturn(accounts);
+		
+		List<Account> accs = accountService.findAll();
+		assertEquals("John", accs.getFirst().getFirstname());
+		
+	}
+	
+	@Test
+	public void findUniqueByUsernameShouldReturnAccount() throws NotFoundException {
+		
+		List<Role> roles = Arrays.asList(new Role(1, "Client"));
+		Account account = new Account(1, "John", "Doe", "johndoe", "mdpJohn", roles);
+		String username = "johndoe";
+		
+		when(accountRepository.findByUsername(username)).thenReturn(Optional.of(account));
+		
+		Account acc = accountService.findUniqueByUsername(username);
+		assertNotNull(acc);
+		assertEquals("Doe", acc.getLastname());
+		
+	}
+	
+	@Test
+	public void findUniqueByUsernameShouldThrowException() {
+		
+		String username = "johndoe";
+		when(accountRepository.findByUsername(username)).thenReturn(Optional.empty());
+		assertThrows(
+				NotFoundException.class, 
+				() -> accountService.findUniqueByUsername(username)
+		);
+		
+	}
+
+}
