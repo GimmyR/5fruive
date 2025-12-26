64b9fd0b2a5c46889ff8b9d363ae09f0443adfc0
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-12-26 20:04:23 +0300

    Creating again IsMissingException for card code when it is null and blank

---
+++ src/main/java/mg/fruive/exception/IsMissingException.java
@@ -0,0 +1,12 @@
+package mg.fruive.exception;
+
+@SuppressWarnings("serial")
+public class IsMissingException extends Exception {
+	
+	public IsMissingException(String message) {
+		
+		super(message);
+		
+	}
+
+}
