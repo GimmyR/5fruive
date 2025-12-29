c5c864775ffea4a88fed0b84a508ab2e961516d4
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-12-29 08:07:05 +0300

    Doing integration test on PurchaseDetailController

---
+++ src/test/java/mg/fruive/controller/PurchaseDetailControllerIntegrationTest.java
@@ -0,0 +1,31 @@
+package mg.fruive.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PurchaseDetailControllerIntegrationTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	@WithMockUser(username = "johndoe", roles = "Admin")
+	public void testGetPurchaseDetails() throws Exception {
+		
+		mockMvc.perform(get("/bo/purchases/1"))
+					.andExpect(status().isOk())
+					.andExpect(view().name("purchase-details/index"));
+		
+	}
+
+}
