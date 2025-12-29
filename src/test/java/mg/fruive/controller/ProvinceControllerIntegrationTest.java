62cce0b2032383fb9fd89de51c88ab608f669f09
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-12-29 08:02:24 +0300

    Doing integration test on ProvinceController

---
+++ src/test/java/mg/fruive/controller/ProvinceControllerIntegrationTest.java
@@ -0,0 +1,31 @@
+package mg.fruive.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProvinceControllerIntegrationTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	@WithMockUser(username = "johndoe", roles = "Admin")
+	public void testGetProvinces() throws Exception {
+		
+		mockMvc.perform(get("/bo/provinces"))
+					.andExpect(status().isOk())
+					.andExpect(view().name("provinces/index"));
+		
+	}
+
+}
