82e18106c26d8987eab60e8da1164895228f745f
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-12-29 07:57:30 +0300

    Doing integration test on ProductController

---
+++ src/test/java/mg/fruive/controller/ProductControllerIntegrationTest.java
@@ -0,0 +1,57 @@
+package mg.fruive.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerIntegrationTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testGetHome() throws Exception {
+		
+		mockMvc.perform(get("/"))
+					.andExpect(status().isOk())
+					.andExpect(view().name("home/index"));
+		
+	}
+	
+	@Test
+	public void testGetUniqueProduct() throws Exception {
+		
+		mockMvc.perform(get("/product/10"))
+					.andExpect(status().isOk())
+					.andExpect(view().name("product/index"));
+		
+	}
+	
+	@Test
+	public void testGetProductsShouldRedirect() throws Exception {
+		
+		mockMvc.perform(get("/bo/products"))
+					.andExpect(status().is(302));
+		
+	}
+	
+	@Test
+	@WithMockUser(username = "johndoe", roles = {"Client", "Admin"})
+	public void testGetProducts() throws Exception {
+		
+		mockMvc.perform(get("/bo/products"))
+					.andExpect(status().isOk())
+					.andExpect(view().name("products/index"));
+		
+	}
+
+}
