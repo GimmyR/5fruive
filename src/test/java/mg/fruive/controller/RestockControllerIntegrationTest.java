8ff6c016f00e1d2f484f8077e69366015fb05db6
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-12-31 09:36:05 +0300

    Doing integration tests on RestockController

---
+++ src/test/java/mg/fruive/controller/RestockControllerIntegrationTest.java
@@ -0,0 +1,41 @@
+package mg.fruive.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RestockControllerIntegrationTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	@WithMockUser(username = "johndoe", roles = "Admin")
+	public void testGetRestocks() throws Exception {
+		
+		mockMvc.perform(get("/bo/restocks"))
+					.andExpect(status().isOk())
+					.andExpect(view().name("restock/index"));
+		
+	}
+	
+	@Test
+	@WithMockUser(username = "johndoe", roles = "Admin")
+	public void testRestockByProductId() throws Exception {
+		
+		mockMvc.perform(get("/bo/restock/1"))
+					.andExpect(status().isOk())
+					.andExpect(view().name("restock/save/index"));
+		
+	}
+
+}
